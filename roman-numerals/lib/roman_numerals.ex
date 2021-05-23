c3999bc1b95cccb20b5524f0b63fe9f6f5310000
carlosmorette <carlos.morette@outlook.com>
2021-05-22 22:22:48 -0300

    start roman-numerals

---
+++ roman-numerals/lib/roman_numerals.ex
@@ -0,0 +1,30 @@
+defmodule RomanNumerals do
+  @doc """
+  Convert the number to a roman number.
+  """
+
+  @roman_numbers %{
+    1 => "I",
+    2 => "II",
+    3 => "III",
+    4 => "IV",
+    5 => "V",
+    6 => "VI",
+    9 => "IX",
+    27 => "XXVII",
+    48 => "XLVIII",
+    59 => "LIX",
+    93 => "XCIII",
+    141 => "CXLI",
+    163 => "CLXIII",
+    402 => "CDII",
+    575 => "DLXXV",
+    911 => "CMXI",
+    1024 => "MXXIV",
+    3000 => "MMM"
+  }
+
+  @spec numeral(pos_integer) :: String.t()
+  def numeral(number) do
+  end
+end
--- word-count/lib/word_count.ex
+++ word-count/lib/word_count.ex
@@ -4,7 +4,16 @@ defmodule WordCount do
 
   Words are compared case-insensitively.
   """
+
   @spec count(String.t()) :: map
   def count(sentence) do
+    characters_for_remove = [":", "!!&@$%^&", ","]
+
+    sentence
+    |> String.replace(characters_for_remove, "")
+    |> String.replace("_", " ")
+    |> String.downcase()
+    |> String.split()
+    |> Enum.frequencies()
   end
 end
